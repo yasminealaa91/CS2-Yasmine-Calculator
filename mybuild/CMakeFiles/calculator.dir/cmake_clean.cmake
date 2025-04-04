d0a2d28cd58a92fbe46d910a7a905a3aa7395884
Yasmine <yasmine.alaa@aucegypt.edu>
2025-04-04 12:58:24 +0200

    untracked files added

---
+++ mybuild/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/calculator.cpp.o"
+  "CMakeFiles/calculator.dir/calculator.cpp.o.d"
+  "libcalculator.a"
+  "libcalculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mybuild/CMakeFiles/calculator.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libcalculator.a"
+)
